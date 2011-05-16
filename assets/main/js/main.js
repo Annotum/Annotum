fe34cc62f6eda4c4ed2e75b8a207623765e505cf
David Lawson <dlawson@crowdfavorite.com>
2011-05-16 17:47:48 -0600

    adding annotum changes -ddl

---
+++ assets/main/js/main.js
@@ -0,0 +1,4 @@
+jQuery(document).ready(function($) {
+	$('input[placeholder],textarea[placeholder]').placeholder();
+});
+
--- forms/search.php
+++ forms/search.php
@@ -18,5 +18,5 @@ $s = get_query_var('s');
 
 <form class="search" method="get" action="<?php echo home_url('/'); ?>">
 	<input type="text" name="s" value="<?php echo esc_html($s); ?>" placeholder="<?php _e('Search', 'anno'); ?>" class="type-text" />
-	<button class="type-submit" type="submit"><?php _e('Search', 'anno'); ?></button>
+	<button class="type-submit imr" placeholder="Search" type="submit"><?php _e('Search', 'anno'); ?></button>
 </form>
