0668826c541275029adc30150c6b00dfdcde46b2
David Lawson <dlawson@crowdfavorite.com>
2011-05-17 10:31:34 -0600

    featured carousel renamed to featured posts  added to theme

---
+++ misc/featured-posts.php
@@ -0,0 +1,18 @@
+<div class="featured-posts carousel">
+	<ul>
+		<li>
+			<div class="carousel-item">
+				<img src="example/featured-carousel-thumb-a.jpg" width="270" height="230" alt="Featured Carousel Thumb A" />
+				<h2 class="title">Featured Post Title</h2>
+				<div class="content">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				</div>
+			</div>
+		</li>
+	</ul>
+	<div class="control-panel">
+		2 of 4
+		<a class="previous imr" href="#">Previous</a>
+		<a class="next imr" href="#">Next</a>
+	</div>
+</div>
