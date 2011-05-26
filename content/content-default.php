c93e35c59629ce55fae7edbfc7fb2ad7a202a6ce
David Lawson <dlawson@crowdfavorite.com>
2011-05-26 15:54:33 -0600

    annotum updates
    -ddl

--- content/content-default.php
+++ content/content-default.php
@@ -33,6 +33,10 @@ if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 				<p><i>Lorem ipsum</i> dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			</div>
 		</section>
+		<div class="to-top">
+			<a href="#"><span>Top</span></a>
+		</div>
+		
 		<section class="article-section article-section-results">
 			<h1 class="section-title"><span>Results</span></h1>
 			<div class="content">
