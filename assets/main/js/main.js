f08286c446146345081ac11dfd4334e2438e5846
Gordon Brander <marlfoskr@gmail.com>
2011-05-31 13:46:39 -0600

    Modifying gallery styles to be compatible with jQuery Cycle Lite

--- assets/main/js/main.js
+++ assets/main/js/main.js
@@ -9,6 +9,9 @@
  */
 (function(d,e){Contexe=function(b){var a=b||"html",c=Contexe;if(!(this instanceof c))return a=c.ins,typeof a[b]==="undefined"&&(a[b]=new c(b)),a[b];a==="html"?a=e.getElementsByTagName("html")[0]:a==="body"&&(a=e.getElementsByTagName("body")[0]);this.el=a};Contexe.ins=[];Contexe.prototype={classReg:function(b){return RegExp("(^|\\s+)"+b+"(\\s+|$)")},hasClass:function(b,a){return this.classReg(a).test(b.className)},given:function(b,a,c){this.hasClass(this.el,b)&&a.apply(null,c);return this}};d.Contexe=Contexe;return d})(this,document);
 
+/**
+ * Annotum module
+ */
 var Anno = function($, Exe) {
 	return {
 		/* Run this method first */
@@ -26,7 +29,6 @@ var Anno = function($, Exe) {
 		
 		home: function () {
 			$('#home-featured ul').cycle({
-				height: 500
 			});
 		}
 	};
