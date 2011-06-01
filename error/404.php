51760588147fdc94209d34008c24d1e287555a50
David Lawson <dlawson@crowdfavorite.com>
2011-06-01 09:24:30 -0600

    404 page
    -ddl

--- error/404.php
+++ error/404.php
@@ -12,14 +12,13 @@
 if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
 if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 
-get_header();
+get_header();?>
 
-_e('Sorry, we\'re not sure what you\'re looking for here.', 'anno'); 
-
-cfct_form('search');
-
-get_sidebar();
-
-get_footer();
-
-?>
+<div id="main-body" class="clearfix">
+	<h1 class="section-title">Page Not Found</h1>
+	<?php _e('Sorry, we\'re not sure what you\'re looking for here.', 'anno'); ?> 
+</div><!-- #main-content -->
+<div id="main-sidebar" class="clearfix">
+	<?php get_sidebar(); ?>
+</div><!-- #main-sidebar -->
+<?php get_footer(); ?>
