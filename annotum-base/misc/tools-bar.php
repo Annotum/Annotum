4b828920a537b46ecabc8d5fba89844b733de1b0
Gordon Brander <marlfoskr@gmail.com>
2011-06-16 16:22:49 -0600

    Working Twitter button.

--- annotum-base/misc/tools-bar.php
+++ annotum-base/misc/tools-bar.php
@@ -6,6 +6,12 @@
 		<label>Download:</label> <a href="#">PDF</a>, <a href="#">XML</a>
 	</div>
 	<div class="cell share clearfix">
-		<?php cfct_misc('social-nav'); ?>
+		<div class="social-nav">
+			<ul class="nav">
+				<li><a class="email" href="#">Email</a></li>
+				<li><?php anno_twitter_button(); ?></li>
+				<li><a class="facebook" href="#">Facebook</a></li>
+			</ul>
+		</div>
 	</div>
 </div><!-- .action-bar -->
