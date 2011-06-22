e6fc61f85798f17b7a8dcb9ffe946b6f20f22a00
David Lawson <dlawson@crowdfavorite.com>
2011-06-22 11:26:09 -0600

    Annotum theme layout fixes
    -ddl

--- annotum-base/misc/tools-bar.php
+++ annotum-base/misc/tools-bar.php
@@ -9,7 +9,7 @@
 		</div><!--/.citation-container -->
 	</div>
 	<div class="cell download">
-		<label>Download:</label> <a href="#">PDF</a>, <a href="#">XML</a>
+		<a href="#">PDF</a>, <a href="#">XML</a>
 	</div>
 	<div class="cell share clearfix">
 		<div class="social-nav">
