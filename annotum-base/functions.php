8a1004d60d80f0621b6c2bd416c242dab5b3d2e6
Evan Anderson <ejdanderson@gmail.com>
2011-06-24 15:23:52 -0600

    Adding initital profile page

--- annotum-base/functions.php
+++ annotum-base/functions.php
@@ -23,6 +23,7 @@ include_once(CFCT_PATH.'functions/capabilities.php');
 include_once(CFCT_PATH.'functions/featured-articles.php');
 include_once(CFCT_PATH.'functions/template.php');
 include_once(CFCT_PATH.'functions/widgets.php');
+include_once(CFCT_PATH.'functions/profile.php');
 include_once(CFCT_PATH.'plugins/load.php');
 
 function anno_setup() {
---
+++ annotum-base/functions/profile.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * Array of user meta keys and their labels
+ */  
+global $anno_user_meta;
+$anno_user_meta = array(
+	'_anno_prefix' => _x('Name Prefix', 'form label', 'anno'),
+	'_anno_suffix' => _x('Name Suffix', 'form label', 'anno'),
+	'_anno_degrees' => _x('Degrees', 'form label', 'anno'),
+	'_anno_affiliation' => _x('Affiliation', 'form label', 'anno'),
+);
+
+/**
+ * Adds the menu page to WP.
+ */ 
+function anno_add_profile() {
+	add_users_page( 
+		_x('Annotum Profile', 'admin page title', 'anno'),
+		_x('Annotum Profile', 'admin sidebar menu title', 'anno'),
+		'read',
+		'anno-profile',
+		'anno_profile'
+	);
+}
+add_action('admin_menu', 'anno_add_profile');
+
+/**
+ * User profile markup for Annotum specific items
+ */ 
+function anno_profile() {
+	global $anno_user_meta;
+	$current_user = wp_get_current_user();
+?>
+<div id="anno-profile-page" class="wrap">
+	<h2><?php _ex('Annotum Profile', 'header', 'anno'); ?></h2>
+	<?php if ($_GET['update']) { ?>
+	<div id="message" class="updated below-h2"><p><?php _ex('Profile Updated', 'admin status banner', 'anno'); ?></p></div>
+	<?php } ?>
+	<form method="post" action="<?php echo admin_url(); ?>">
+		<table class="form-table">
+			<tbody>
+				<tr>
+					<th><label for="first_name"><?php _ex('First Name', 'form label', 'anno'); ?></label></th>
+					<td><span id="first_name"><?php echo esc_html($current_user->first_name); ?> &#40;<a href="<?php echo admin_url('profile.php#first_name'); ?>"><?php _ex('Change', 'profile link text', 'anno'); ?>&#41;</span></td>
+				</tr>
+				
+				<tr>
+					<th><label for="last_name"><?php _ex('Last Name', 'form label', 'anno'); ?></label></th>
+					<td><span id="user_login"><?php echo esc_html($current_user->last_name); ?> &#40;<a href="<?php echo admin_url('profile.php#last_name'); ?>"><?php _ex('Change', 'profile link text', 'anno'); ?>&#41;</span></td>
+				</tr>
+				
+				
+<?php
+	foreach ($anno_user_meta as $key => $label) {
+		$meta_val = get_user_meta($current_user->ID, $key, true);
+?>
+				<tr>
+					<th><label for="<?php echo $key; ?>"><?php echo $label; ?></label></th>
+					<td><input type="text" name="<?php echo $key; ?>" class="regular-text" id="<?php echo $key; ?>" value="<?php echo esc_attr($meta_val); ?>" />
+				</tr>
+<?php
+	}
+?>
+			</tbody>
+		</table>
+		<p class="submit">
+			<?php echo wp_nonce_field('anno_profile', 'anno_profile_nonce', true, false); ?>
+			<input type="hidden" name="anno_action" value="update_profile" />
+			<input type="hidden" name="user_id" value="<?php echo $current_user->ID; ?>">
+			<input type="submit" value="<?php _ex('Update Profile', 'button label', 'anno'); ?>" class="button-primary" id="submit" />
+		</p>
+	</form>
+</div>
+<?php	
+}
+
+/**
+ * User profile request handler. Handles saving of user meta.
+ */ 
+function anno_profile_request_handler() {
+	if (isset($_POST['anno_action']) && isset($_POST['user_id']) && $_POST['anno_action'] == 'update_profile') {
+		if (!check_admin_referer('anno_profile', 'anno_profile_nonce')) {
+			die();
+		}
+		global $anno_user_meta;
+		foreach ($anno_user_meta as $meta_key => $label) {
+			if (isset($_POST[$meta_key])) {
+				$value = trim($_POST[$meta_key]);
+			}
+			else {
+				$value = '';
+			}
+			update_user_meta(absint($_POST['user_id']), $meta_key, $value);
+		}
+		
+		wp_redirect(admin_url('users.php?page=anno-profile&update=true'));
+		die();
+	}
+}
+add_action('init', 'anno_profile_request_handler', 0);
+
+?>
