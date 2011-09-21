0716b20798ed0a5d6b87ef49c26ffb7d63663eaa
Matt Richmond <matt@crowdfavorite.com>
2011-09-21 16:35:05 -0600

    define the DOMPDF media stuff

--- annotum-base/plugins/anno-pdf-download/anno-pdf-download.php
+++ annotum-base/plugins/anno-pdf-download/anno-pdf-download.php
@@ -53,8 +53,8 @@ class Anno_PDF_Download {
 		//define("DOMPDF_UNICODE_ENABLED", false);
 		//define("TTF2AFM", "C:/Program Files (x86)/GnuWin32/bin/ttf2pt1.exe");
 		//define("DOMPDF_PDF_BACKEND", "PDFLib");
-		//define("DOMPDF_DEFAULT_MEDIA_TYPE", "print");
-		//define("DOMPDF_DEFAULT_PAPER_SIZE", "letter");
+		define("DOMPDF_DEFAULT_MEDIA_TYPE", "print");
+		define("DOMPDF_DEFAULT_PAPER_SIZE", "letter");
 		//define("DOMPDF_DEFAULT_FONT", "serif");
 		//define("DOMPDF_DPI", 72);
 		//define("DOMPDF_ENABLE_PHP", true);
