5f04cc25db02a39aa5ed476ce740e32899021ec9
Evan Anderson <ejdanderson@gmail.com>
2011-09-22 17:39:55 -0600

    P tags in captions from the get go

--- annotum-base/js/tinymce/plugins/annoimages/popup.js
+++ annotum-base/js/tinymce/plugins/annoimages/popup.js
@@ -62,7 +62,7 @@ jQuery(document).ready( function($) {
 			xml = '<fig>'
 						+'<img src="' + url + '" />'
 						+'<label>' + label + '</label>'
-						+'<cap>' + caption + '</cap>'
+						+'<cap><p>' + caption + '</p></cap>'
 						+'<media xlink:href="' + url + '">'
 							+'<alt-text>' + alt_text + '</alt-text>'
 							+'<long-desc>' + description + '</long-desc>'
