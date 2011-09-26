6c6a507754326990edf969c8b7f26246d46559a2
Matt Richmond <matt@crowdfavorite.com>
2011-09-26 13:29:43 -0600

    Changing template to be HTML4, so DOMPDF likes it better

--- annotum-base/plugins/anno-pdf-download/templates/default.php
+++ annotum-base/plugins/anno-pdf-download/templates/default.php
@@ -1,7 +1,57 @@
-<?php get_header();?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=<?php echo esc_attr(get_bloginfo('charset')); ?>">
+	
+	<title><?php wp_title( '-', true, 'right' ); echo esc_html( get_bloginfo('name') ); ?></title>
 
-<div id="main-body" class="clearfix">
-	<?php cfct_template_file('content', 'type-article'); ?>
-</div><!-- #main-content -->
+	<link rel='stylesheet' id='anno-css'  href='http://annotum.local/wp-content/themes/Annotum/annotum-base/assets/main/css/main.css?ver=1.1' type='text/css' media='all'>
+	
+	<?php cfct_misc('custom-colors'); ?>
+	
+	<style type="text/css" media="all">
+		body {
+			margin: 20px;
+/*			color: #000 !important;*/
+		}
+		.sub,
+		.sup{
+			font-size:75%;
+			line-height:0;
+			position:relative;
+		}
+		.sup{
+			top:-0.5em;
+		}
+		.sub{
+			bottom:-0.25em;
+		}
+		.figcaption {
+			font-size:13px;
+			line-height:1.384615385;
+			overflow:hidden;
+		}
+		div.tools-bar {
+			display:none;
+		}
+		#wpadminbar {
+			display:none;
+		}
+	</style>
+</head>
 
-<?php get_footer(); ?>
+<body <?php body_class(); ?>>
+	<div id="main" class="act">
+		<div class="in">
+		
+			<div id="main-body" class="clearfix">
+				
+				<?php cfct_template_file('content', 'type-article-html4'); ?>
+				
+			</div><!-- #main-content -->
+
+		</div><!-- .in -->
+	</div><!-- #main -->
+	<?php wp_footer(); ?>
+</body>
+</html>
