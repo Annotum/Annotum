3394413291e736d0466a0806b90814f7fa1946f4
carlthewebmaster <carl@solvitor.com>
2011-09-29 16:36:43 -0400

    added buttonless fullscreen plugin for TinyMCE. Alt+Shift+G to open or close

--- annotum-base/functions/tinymce.php
+++ annotum-base/functions/tinymce.php
@@ -257,7 +257,9 @@ class Anno_tinyMCE {
 			
 			$plugins['annoEquations'] = trailingslashit(get_bloginfo('template_directory')).'js/tinymce/plugins/annoequations/editor_plugin.js';
 			
-		//	$plugins['annoPaste'] = trailingslashit(get_bloginfo('template_directory')).'js/tinymce/plugins/annopaste/editor_plugin.js';		
+			$plugins['fullscreen'] = trailingslashit(get_bloginfo('template_directory')).'js/tinymce/plugins/fullscreen/editor_plugin.js';
+
+			//	$plugins['annoPaste'] = trailingslashit(get_bloginfo('template_directory')).'js/tinymce/plugins/annopaste/editor_plugin.js';		
 		}
 		return $plugins;
 	}
