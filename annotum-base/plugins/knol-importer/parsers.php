b4d677650861419d1b4101f8e3319264609f5f34
Evan Anderson <ejdanderson@gmail.com>
2011-10-03 15:57:33 -0600

    Multi channel support for simple xml parser

--- annotum-base/plugins/knol-importer/parsers.php
+++ annotum-base/plugins/knol-importer/parsers.php
@@ -75,136 +75,138 @@ class Knol_WXR_Parser_SimpleXML {
 		if ( ! isset( $namespaces['excerpt'] ) )
 			$namespaces['excerpt'] = 'http://wordpress.org/export/1.1/excerpt/';
 
-		// grab authors
-		foreach ( $xml->xpath('/rss/channel/wp:author') as $author_arr ) {
-			$a = $author_arr->children( $namespaces['wp'] );
-			// Knol WXR has Creator stored by 'Knol ID' (author_id) not author_login. author_login is empty in the WXR.
-			$author_key = (string) $a->author_id;
-			$authors[$author_key] = array(
-				'author_id' => (string) $a->author_id,
-				'author_login' => $a->author_login,
-				'author_email' => (string) $a->author_email,
-				'author_display_name' => (string) $a->author_display_name,
-				'author_first_name' => (string) $a->author_first_name,
-				'author_last_name' => (string) $a->author_last_name
-			);
-		}
-
-		// grab cats, tags and terms
-		foreach ( $xml->xpath('/rss/channel/wp:category') as $term_arr ) {
-			$t = $term_arr->children( $namespaces['wp'] );
-			$categories[] = array(
-				'term_id' => (int) $t->term_id,
-				'category_nicename' => (string) $t->category_nicename,
-				'category_parent' => (string) $t->category_parent,
-				'cat_name' => (string) $t->cat_name,
-				'category_description' => (string) $t->category_description
-			);
-		}
-
-		foreach ( $xml->xpath('/rss/channel/wp:tag') as $term_arr ) {
-			$t = $term_arr->children( $namespaces['wp'] );
-			$tags[] = array(
-				'term_id' => (int) $t->term_id,
-				'tag_slug' => (string) $t->tag_slug,
-				'tag_name' => (string) $t->tag_name,
-				'tag_description' => (string) $t->tag_description
-			);
-		}
-
-		foreach ( $xml->xpath('/rss/channel/wp:term') as $term_arr ) {
-			$t = $term_arr->children( $namespaces['wp'] );
-			$terms[] = array(
-				'term_id' => (int) $t->term_id,
-				'term_taxonomy' => (string) $t->term_taxonomy,
-				'slug' => (string) $t->term_slug,
-				'term_parent' => (string) $t->term_parent,
-				'term_name' => (string) $t->term_name,
-				'term_description' => (string) $t->term_description
-			);
-		}
-
-		// grab posts
-		foreach ( $xml->channel->item as $item ) {
-			$post = array(
-				'post_title' => (string) $item->title,
-				'guid' => (string) $item->guid,
-			);
-
-			$dc = $item->children( 'http://purl.org/dc/elements/1.1/' );
-			$post['post_author'] = (string) $dc->creator;
-
-			$content = $item->children( 'http://purl.org/rss/1.0/modules/content/' );
-			$excerpt = $item->children( $namespaces['excerpt'] );
-			$content_filtered = $item->children( $namespaces['content_filtered'] );
-			
-			$post['post_content'] = (string) $content->encoded;
-			$post['post_excerpt'] = (string) $excerpt->encoded;
-			$post['post_content_filtered'] = (string) $content_filtered->encoded;
-			
-			$wp = $item->children( $namespaces['wp'] );
-			$post['post_id'] = (int) $wp->post_id;
-			$post['post_date'] = (string) $wp->post_date;
-			$post['post_date_gmt'] = (string) $wp->post_date_gmt;
-			$post['comment_status'] = (string) $wp->comment_status;
-			$post['ping_status'] = (string) $wp->ping_status;
-			$post['post_name'] = (string) $wp->post_name;
-			$post['status'] = (string) $wp->status;
-			$post['post_parent'] = (int) $wp->post_parent;
-			$post['menu_order'] = (int) $wp->menu_order;
-			$post['post_type'] = (string) $wp->post_type;
-			$post['post_password'] = (string) $wp->post_password;
-			$post['is_sticky'] = (int) $wp->is_sticky;
-
-			if ( isset($wp->attachment_url) )
-				$post['attachment_url'] = (string) $wp->attachment_url;
-
-			foreach ( $item->category as $c ) {
-				$att = $c->attributes();
-				if ( isset( $att['nicename'] ) )
-					$post['terms'][] = array(
-						'name' => (string) $c,
-						'slug' => (string) $att['nicename'],
-						'domain' => (string) $att['domain']
-					);
-			}
-
-			foreach ( $wp->postmeta as $meta ) {
-				$post['postmeta'][] = array(
-					'key' => (string) $meta->meta_key,
-					'value' => (string) $meta->meta_value
+		foreach ($xml->xpath('/rss/channel') as $channel) {
+			// grab authors
+			foreach ( $channel->xpath('wp:author') as $author_arr ) {
+				$a = $author_arr->children( $namespaces['wp'] );
+				// Knol WXR has Creator stored by 'Knol ID' (author_id) not author_login. author_login is empty in the WXR.
+				$author_key = (string) $a->author_id;
+				$authors[$author_key] = array(
+					'author_id' => (string) $a->author_id,
+					'author_login' => $a->author_login,
+					'author_email' => (string) $a->author_email,
+					'author_display_name' => (string) $a->author_display_name,
+					'author_first_name' => (string) $a->author_first_name,
+					'author_last_name' => (string) $a->author_last_name
 				);
 			}
 
-			foreach ( $wp->comment as $comment ) {
-				$meta = array();
-				if ( isset( $comment->commentmeta ) ) {
-					foreach ( $comment->commentmeta as $m ) {
-						$meta[] = array(
-							'key' => (string) $m->meta_key,
-							'value' => (string) $m->meta_value
+			// grab cats, tags and terms
+			foreach ( $channel->xpath('wp:category') as $term_arr ) {
+				$t = $term_arr->children( $namespaces['wp'] );
+				$categories[] = array(
+					'term_id' => (int) $t->term_id,
+					'category_nicename' => (string) $t->category_nicename,
+					'category_parent' => (string) $t->category_parent,
+					'cat_name' => (string) $t->cat_name,
+					'category_description' => (string) $t->category_description
+				);
+			}
+
+			foreach ( $channel->xpath('wp:tag') as $term_arr ) {
+				$t = $term_arr->children( $namespaces['wp'] );
+				$tags[] = array(
+					'term_id' => (int) $t->term_id,
+					'tag_slug' => (string) $t->tag_slug,
+					'tag_name' => (string) $t->tag_name,
+					'tag_description' => (string) $t->tag_description
+				);
+			}
+
+			foreach ( $channel->xpath('wp:term') as $term_arr ) {
+				$t = $term_arr->children( $namespaces['wp'] );
+				$terms[] = array(
+					'term_id' => (int) $t->term_id,
+					'term_taxonomy' => (string) $t->term_taxonomy,
+					'slug' => (string) $t->term_slug,
+					'term_parent' => (string) $t->term_parent,
+					'term_name' => (string) $t->term_name,
+					'term_description' => (string) $t->term_description
+				);
+			}
+
+			// grab posts
+			foreach ( $channel->item as $item ) {
+				$post = array(
+					'post_title' => (string) $item->title,
+					'guid' => (string) $item->guid,
+				);
+
+				$dc = $item->children( 'http://purl.org/dc/elements/1.1/' );
+				$post['post_author'] = (string) $dc->creator;
+
+				$content = $item->children( 'http://purl.org/rss/1.0/modules/content/' );
+				$excerpt = $item->children( $namespaces['excerpt'] );
+				$content_filtered = $item->children( $namespaces['content_filtered'] );
+			
+				$post['post_content'] = (string) $content->encoded;
+				$post['post_excerpt'] = (string) $excerpt->encoded;
+				$post['post_content_filtered'] = (string) $content_filtered->encoded;
+			
+				$wp = $item->children( $namespaces['wp'] );
+				$post['post_id'] = (int) $wp->post_id;
+				$post['post_date'] = (string) $wp->post_date;
+				$post['post_date_gmt'] = (string) $wp->post_date_gmt;
+				$post['comment_status'] = (string) $wp->comment_status;
+				$post['ping_status'] = (string) $wp->ping_status;
+				$post['post_name'] = (string) $wp->post_name;
+				$post['status'] = (string) $wp->status;
+				$post['post_parent'] = (int) $wp->post_parent;
+				$post['menu_order'] = (int) $wp->menu_order;
+				$post['post_type'] = (string) $wp->post_type;
+				$post['post_password'] = (string) $wp->post_password;
+				$post['is_sticky'] = (int) $wp->is_sticky;
+
+				if ( isset($wp->attachment_url) )
+					$post['attachment_url'] = (string) $wp->attachment_url;
+
+				foreach ( $item->category as $c ) {
+					$att = $c->attributes();
+					if ( isset( $att['nicename'] ) )
+						$post['terms'][] = array(
+							'name' => (string) $c,
+							'slug' => (string) $att['nicename'],
+							'domain' => (string) $att['domain']
 						);
-					}
 				}
-			
-				$post['comments'][] = array(
-					'comment_id' => (int) $comment->comment_id,
-					'comment_author' => (string) $comment->comment_author,
-					'comment_author_email' => (string) $comment->comment_author_email,
-					'comment_author_IP' => (string) $comment->comment_author_IP,
-					'comment_author_url' => (string) $comment->comment_author_url,
-					'comment_date' => (string) $comment->comment_date,
-					'comment_date_gmt' => (string) $comment->comment_date_gmt,
-					'comment_content' => (string) $comment->comment_content,
-					'comment_approved' => (string) $comment->comment_approved,
-					'comment_type' => (string) $comment->comment_type,
-					'comment_parent' => (string) $comment->comment_parent,
-					'comment_user_id' => (int) $comment->comment_user_id,
-					'commentmeta' => $meta,
-				);
-			}
 
-			$posts[] = $post;
+				foreach ( $wp->postmeta as $meta ) {
+					$post['postmeta'][] = array(
+						'key' => (string) $meta->meta_key,
+						'value' => (string) $meta->meta_value
+					);
+				}
+
+				foreach ( $wp->comment as $comment ) {
+					$meta = array();
+					if ( isset( $comment->commentmeta ) ) {
+						foreach ( $comment->commentmeta as $m ) {
+							$meta[] = array(
+								'key' => (string) $m->meta_key,
+								'value' => (string) $m->meta_value
+							);
+						}
+					}
+			
+					$post['comments'][] = array(
+						'comment_id' => (int) $comment->comment_id,
+						'comment_author' => (string) $comment->comment_author,
+						'comment_author_email' => (string) $comment->comment_author_email,
+						'comment_author_IP' => (string) $comment->comment_author_IP,
+						'comment_author_url' => (string) $comment->comment_author_url,
+						'comment_date' => (string) $comment->comment_date,
+						'comment_date_gmt' => (string) $comment->comment_date_gmt,
+						'comment_content' => (string) $comment->comment_content,
+						'comment_approved' => (string) $comment->comment_approved,
+						'comment_type' => (string) $comment->comment_type,
+						'comment_parent' => (string) $comment->comment_parent,
+						'comment_user_id' => (int) $comment->comment_user_id,
+						'commentmeta' => $meta,
+					);
+				}
+
+				$posts[] = $post;
+			}
 		}
 
 		return array(
