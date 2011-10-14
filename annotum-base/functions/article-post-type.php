1532bde4b5e3f06d1aa2117a228e97eaaa5c2b7e
Evan Anderson <ejdanderson@gmail.com>
2011-10-14 13:25:27 -0600

    Better formatting for tinyMCE old WP error message

--- annotum-base/functions/article-post-type.php
+++ annotum-base/functions/article-post-type.php
@@ -134,7 +134,7 @@ function anno_body_meta_box($post) {
 		anno_load_editor($content, 'anno-body');
 	}
 	else {
-		echo sprintf(_x('The Annotum editor requires at least WordPress 3.3. It appears you are using WordPress %s. ', 'WordPress version error message', 'anno'), get_bloginfo('version'));
+		echo '<p style="padding:0 10px;">'.sprintf(_x('The Annotum editor requires at least WordPress 3.3. It appears you are using WordPress %s. ', 'WordPress version error message', 'anno'), get_bloginfo('version')).'</p>';
 	}
 ?>
 
@@ -157,7 +157,7 @@ function anno_references_meta_box($post) {
 		}
 	}
 	else {
-		echo sprintf(_x('The Annotum editor requires at least WordPress 3.3. It appears you are using WordPress %s. ', 'WordPress version error message', 'anno'), get_bloginfo('version'));
+		echo '<p>'.sprintf(_x('The Annotum editor requires at least WordPress 3.3. It appears you are using WordPress %s. ', 'WordPress version error message', 'anno'), get_bloginfo('version')).'</p>';
 	}
 	
 }
