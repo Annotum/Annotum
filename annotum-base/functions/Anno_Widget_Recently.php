652c1779983458e812914a6b82bcbb6076586e2d
Devin Reams <devin@crowdfavorite.com>
2011-11-17 15:13:14 -0700

    change copyright to 2011

--- annotum-base/functions/Anno_Widget_Recently.php
+++ annotum-base/functions/Anno_Widget_Recently.php
@@ -4,7 +4,7 @@
  * This file is part of the Annotum theme for WordPress
  * Built on the Carrington theme framework <http://carringtontheme.com>
  *
- * Copyright 2008-2010 Crowd Favorite, Ltd. All rights reserved. <http://crowdfavorite.com>
+ * Copyright 2008-2011 Crowd Favorite, Ltd. All rights reserved. <http://crowdfavorite.com>
  * Released under the GPL license
  * http://www.opensource.org/licenses/gpl-license.php
  * 
