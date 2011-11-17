4787d9c6132d4139ef8c6216e463272f75d56bfa
Ryan Glaze <rglaze@Ryans-MacBook-Pro.local>
2011-11-17 11:28:57 -0700

    Working through bugs

--- annotum-base/functions.php
+++ annotum-base/functions.php
@@ -980,4 +980,24 @@ function anno_default_widgets() {
 	}
 }
 
+function improved_trim_excerpt($text) {
+        global $post;
+        if ( '' == $text ) {
+                $text = get_the_content('');
+                $text = apply_filters('the_content', $text);
+                $text = str_replace('\]\]\>', ']]&gt;', $text);
+                $text = preg_replace('@<script[^>]*?>.*?</script>@si', '', $text);
+                $excerpt_length = 55;
+                $words = explode(' ', $text, $excerpt_length + 1);
+                if (count($words)> $excerpt_length) {
+                        array_pop($words);
+                        array_push($words, '[...]');
+                        $text = implode(' ', $words);
+                }
+        }
+        return $text;
+}
+remove_filter('get_the_excerpt', 'wp_trim_excerpt');
+add_filter('get_the_excerpt', 'improved_trim_excerpt');
+
 ?>
