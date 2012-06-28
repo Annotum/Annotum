0f7f6ad6ace356cdbce200a70141c7ee5879e860
Evan Anderson <ejdanderson@gmail.com>
2012-06-28 09:52:41 -0600

    Markup changes for citation, adding citation output to PDF

--- annotum-base/plugins/anno-pdf-download/templates/default.php
+++ annotum-base/plugins/anno-pdf-download/templates/default.php
@@ -6,16 +6,40 @@
 	<title><?php wp_title( '-', true, 'right' ); echo esc_html( get_bloginfo('name') ); ?></title>
 
 	<link rel="stylesheet" id="anno-pdf-css"  href="<?php echo esc_url(trailingslashit(get_template_directory_uri()).'assets/main/css/pdf.css?ver=1.1'); ?>" type="text/css" media="all">
+	<style type="text/css">
+
+	#header,
+	#footer {
+	  position: fixed;
+	  left: 0;
+		right: 0;
+		color: #aaa;
+		font-size: 0.9em;
+	}
+
+	#header {
+	  top: 0;
+		border-bottom: 0.1pt solid #aaa;
+	}
+	</style>
 </head>
 
 <body <?php body_class(); ?>>
 	
+	<div id="header">
+	  <table>
+	    <tr>
+	      <td>Example document</td>
+	      <td style="text-align: right;">Author</td>
+	    </tr>
+	  </table>
+	</div>
 	<div id="main" class="act">
 		<div class="in">
 		
 			<div id="main-body" class="clearfix">
 				
-				<?php cfct_template_file('content', 'type-article'); ?>
+				<?php cfct_template_file('content', 'article-pdf'); ?>
 				
 			</div><!-- #main-content -->
 
