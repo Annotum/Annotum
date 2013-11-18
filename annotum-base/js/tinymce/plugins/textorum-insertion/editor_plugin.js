c2d2dc75846c3e8ba2ec48381d2469eb9488a6a0
Jonathan Johnson <jon@crowdfavorite.com>
2013-11-18 12:10:27 -0700

    enable / disable button in step with image

--- annotum-base/js/tinymce/plugins/textorum-insertion/editor_plugin.js
+++ annotum-base/js/tinymce/plugins/textorum-insertion/editor_plugin.js
@@ -129,8 +129,10 @@
 
 				if (options.indexOf('inline-graphic') === -1 || options.indexOf('fig') === -1) {
 					cm.get('annoimages').setDisabled(true);
+					cm.get('annoequation').setDisabled(true);
 				}
 				else {
+					cm.get('annoimages').setDisabled(false);
 					cm.get('annoimages').setDisabled(false);
 				}
 
