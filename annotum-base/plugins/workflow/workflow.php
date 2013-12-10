211b54760d6541b388c857ad3283bd62855609c3
Evan Anderson <ejdanderson@gmail.com>
2013-12-10 14:13:46 -0700

    Remove WP revisions meta box, fixes #75

--- annotum-base/plugins/workflow/workflow.php
+++ annotum-base/plugins/workflow/workflow.php
@@ -23,36 +23,39 @@ $annowf_states = array(
 
 /**
  * Remove/Add meta boxes
- */ 
+ */
 function annowf_meta_boxes() {
 	global $post;
 	$post_state = annowf_get_post_state($post->ID);
-	
+
 	// Remove the WP Publish box
 	remove_meta_box('submitdiv', 'article', 'side');
 
 	// Remove author box
 	remove_meta_box('authordiv', 'article', 'normal');
-		
+
 	// Remove slug div
 	remove_meta_box('slugdiv', 'article', 'normal');
-	
+
+	// Built in WP Revisions...
+	remove_meta_box('revisionsdiv', 'article', 'normal');
+
 	if (!anno_user_can('manage_public_comments', null, $post->ID)) {
 		remove_meta_box('commentsdiv', 'article', 'normal');
 	}
-	
+
 	// Remove Revisions box in favor of audit log box
 	if (anno_user_can('view_audit')) {
 		add_meta_box('audit_log', _x('Audit Log', 'Meta box title', 'anno'), 'annowf_audit_log', 'article', 'normal', 'low');
 	}
-	
+
 	// Remove taxonomy, edit, featured, boxes when a user is unable to save/edit
 	if (!anno_user_can('edit_post', null, $post->ID)) {
 		remove_meta_box('tagsdiv-article_tag', 'article', 'side');
 		remove_meta_box('article_category_select', 'article', 'side');
 		remove_meta_box('postimagediv', 'article', 'side', 'low');
 	}
-	
+
 	//@TODO potential role hook here
 	if (!(current_user_can('editor') || current_user_can('administrator'))) {
 		remove_meta_box('commentstatusdiv', 'article', 'normal');
@@ -61,7 +64,7 @@ function annowf_meta_boxes() {
 	// Custom author select box. Only displays co-authors in the dropdown.
 
 	add_meta_box('authordiv', _x('Author', 'Meta box title', 'anno'), 'annowf_author_meta_box', 'article', 'side', 'low');
-	
+
 	// Add the Annotum workflow publish box.
 	global $annowf_states;
 	add_meta_box('submitdiv', _x('Status:', 'Meta box title', 'anno').' '. esc_html($annowf_states[$post_state]), 'annowf_status_meta_box', 'article', 'side', 'high');
@@ -90,7 +93,7 @@ add_action('admin_print_styles-post.php', 'annowf_css');
 function annowf_js() {
 	wp_enqueue_script('suggest');
 	wp_enqueue_script('anno-workflow', trailingslashit(get_template_directory_uri()).'plugins/workflow/js/workflow.js', array('jquery', 'suggest'));
-	
+
 	// Remove Auto-Save feature if a user cannot edit a post. *Note this prevents previewing inputted markup
 	if (!anno_user_can('edit_post')) {
 		wp_dequeue_script('autosave');
@@ -101,10 +104,10 @@ add_action('admin_print_scripts-post.php', 'annowf_js');
 
 /**
  * Article Save action for correcting WP status updating. Fires before insertion into DB
- */ 
+ */
 function annowf_insert_post_data($post, $postarr) {
 	if ($post['post_type'] == 'article' && $post['post_status'] != 'auto-draft') {
-		
+
 		global $anno_post_save;
 		if (isset($_POST['revert']) && $_POST['revert'] == $anno_post_save['revert']) {
 			$post['post_status'] = 'draft';
@@ -121,7 +124,7 @@ function annowf_insert_post_data($post, $postarr) {
 				case $anno_post_save['approve']:
 				case $anno_post_save['review']:
 					$post['post_status'] = 'pending';
-					break;	
+					break;
 				default:
 					break;
 			}
@@ -133,19 +136,19 @@ add_filter('wp_insert_post_data', 'annowf_insert_post_data', 10, 2);
 
 /**
  * Article state transistioning. Fires after post has been inserted into the database
- */ 
+ */
 function annowf_transistion_state($post_id, $post, $post_before) {
 
 	if ($post->post_type == 'article' && $post->post_status != 'auto-draft') {
 		global $anno_post_save;
-		$current_user = wp_get_current_user();		
+		$current_user = wp_get_current_user();
 		$old_state = get_post_meta($post->ID, '_post_state', true);
 		$new_state = $old_state;
-		
+
 		if (empty($old_state)) {
 			$old_state = 'draft';
 		}
-		
+
 		if (isset($_POST['revert']) && $_POST['revert'] == $anno_post_save['revert']) {
 			$new_state = 'draft';
 		}
@@ -177,7 +180,7 @@ function annowf_transistion_state($post_id, $post, $post_before) {
 						$new_state = 'published';
 					}
 					break;
-				case $anno_post_save['review']:	
+				case $anno_post_save['review']:
 					$reviewers = anno_get_reviewers($post->ID);
 					if (is_array($reviewers) && count($reviewers) && in_array($old_state, array('submitted', 'draft'))) {
 						$new_state = 'in_review';
@@ -190,7 +193,7 @@ function annowf_transistion_state($post_id, $post, $post_before) {
 					break;
 			}
 		}
-		
+
 		$notification_type = $new_state;
 		if ($old_state == 'draft' && $new_state == 'in_review') {
 			$notification_type = 're_review';
@@ -201,17 +204,17 @@ function annowf_transistion_state($post_id, $post, $post_before) {
 			update_post_meta($post_id, '_round', intval($round) + 1);
 			$notification_type = 'revisions';
 		}
-		
+
 		if ($new_state != $old_state) {
 			if (empty($new_state)) {
 				$new_state = 'draft';
 			}
 			update_post_meta($post->ID, '_post_state', $new_state);
 			do_action('anno_state_change', $new_state, $old_state);
-			
+
 			// Save to audit log
 			annowf_save_audit_item($post->ID, $current_user->ID, 2, array($old_state, $new_state));
-			
+
 			// Send notifications, but not for published to draft state
 			if (anno_workflow_enabled('notifications') && !($old_state == 'published' && $new_state == 'draft')) {
 				annowf_send_notification($notification_type, $post);
@@ -252,7 +255,7 @@ add_action('post_updated', 'annowf_switch_authors', 10, 3);
 function anno_put_post_revision($rev_id) {
 	$current_user = wp_get_current_user();
 	$revision = get_post($rev_id);
-	
+
 	if (!empty($revision)) {
 		$post = get_post($revision->post_parent);
 	}
@@ -285,7 +288,7 @@ function annowf_create_user_meta_markup($type) {
  * Display base markup for user management meta boxes if type param is equal to co_author or reviewer
  * @param string $type The type of user management being displayed
  * @return void
- */ 
+ */
 function annowf_user_management_meta_box_markup_start($type, $post) {
 	if (!in_array($type, array('co_author', 'reviewer'))) {
 		return;
@@ -299,13 +302,13 @@ function annowf_user_management_meta_box_markup_start($type, $post) {
 		<?php annowf_create_user_meta_markup($type); ?>
 		<?php if (anno_user_can('manage_'.$type_plural, null, $post->ID)) { ?>
 			<div id="<?php echo esc_attr('anno-user-input-'.$type); ?>" class="anno-user-add-wrap">
-				<input type="text" id="<?php echo esc_attr($type.'-input'); ?>" class="user-input" name="<?php echo esc_attr($type.'_input'); ?>" /> 
+				<input type="text" id="<?php echo esc_attr($type.'-input'); ?>" class="user-input" name="<?php echo esc_attr($type.'_input'); ?>" />
 				<input id="<?php echo esc_attr($type.'-add'); ?>" class="user-add button" type="button" value="add" />
 				<?php wp_nonce_field('anno_manage_'.$type, '_ajax_nonce-manage-'.$type, false); ?>
 				<p>
 				 	<?php _ex('or <a href="#" class="'.esc_attr('anno-show-create-'.$type).'">invite a new user</a>', 'invite user link', 'anno'); ?>
 				</p>
-			</div>			
+			</div>
 		<?php } ?>
 <?php
 }
@@ -329,7 +332,7 @@ function annowf_reviewers_meta_box($post) {
 			delete_post_meta($post->ID, '_anno_reviewer_'.$user_id);
 		}
 	}
-		
+
 ?>
 		</ul>
 	</div><!-- reviewer-meta-box -->
@@ -372,10 +375,10 @@ function annowf_co_authors_meta_box($post) {
 function annowf_user_li_markup($user, $type = null) {
 	$post_id = anno_get_post_id();
 	$extra = '&nbsp;';
-	
+
 	// expecting reviewer or co_author
 	$type_plural = $type.'s';
-	
+
 	if ($type == 'reviewer' && anno_user_can('manage_'.$type_plural, null, $post_id)) {
 		// If the type is a user, show what review they left, if any. Loaded in $extra
 		global $anno_review_options;
@@ -387,7 +390,7 @@ function annowf_user_li_markup($user, $type = null) {
 		}
 	}
 	$remove = '&nbsp;';
-	
+
 	// If a user can manage this type of user, show the remove link.
 	if (anno_user_can('manage_'.$type_plural, null, $post_id)) {
 		$remove = '&nbsp;&nbsp;<a href="#" class="anno-user-remove">'._x('remove', 'remove user link for admin screens', 'anno').'</a>';
@@ -405,17 +408,17 @@ function annowf_user_li_markup($user, $type = null) {
 /**
  * Invite a user as a contributor via POST vars.
  * AJAX handler for adding users.
- */ 
+ */
 function annowf_invite_user() {
 	check_ajax_referer('anno_create_user', '_ajax_nonce-create-user');
-	
+
 	// Array for json response
 	$data_array = array(
 		'code' => '',
 		'message' => '',
 		'user' => '',
 	);
-	
+
 	// Only allow entering of email addresses, so a user can't create 'bobisdumb' as the username
 	$user_id = anno_invite_contributor($_POST['user_email'], $_POST['user_email']);
 
@@ -438,25 +441,25 @@ function annowf_invite_user() {
 	}
 	echo json_encode($data_array);
 	die();
-	
+
 }
 add_action('wp_ajax_anno-invite-user', 'annowf_invite_user');
 
 /**
  * Handles AJAX request for adding a reviewer to a post. As well as transitioning states.
- */ 
+ */
 function annowf_add_reviewer() {
 	$response = annowf_add_user('reviewer');
 	if ($response['message'] == 'success') {
 		$post_id = absint($_POST['post_id']);
 		$post_state = annowf_get_post_state($post_id);
-	
+
 		//Send email
 		if (anno_workflow_enabled('notifications')) {
 			$post = get_post($post_id);
 			annowf_send_notification('reviewer_added', $post, '', array($response['user']->user_email), $response['user']);
 		}
-		
+
 		if ($post_state == 'submitted') {
 			update_post_meta($post_id, '_post_state', 'in_review');
 			if (anno_workflow_enabled('notifications')) {
@@ -464,7 +467,7 @@ function annowf_add_reviewer() {
 				annowf_send_notification('in_review', $post);
 			}
 		}
-		
+
 		// If the reviewer is being re-added and has already left a review for this round
 		$round = annowf_get_round($post_id);
 
@@ -474,18 +477,18 @@ function annowf_add_reviewer() {
 			$reviewed = get_post_meta($post_id, '_round_'.$round.'_reviewed', true);
 			$reviewed[] = $response['user']->ID;
 			update_post_meta($post_id, '_round_'.$round.'_reviewed', array_unique($reviewed));
-			
+
 			// Used for incrementation of x of x reviewed
 			$response['increment'] = 1;
 		}
 		else {
 			$response['increment'] = 0;
 		}
-		
+
 		//Add to the audit log
 		$current_user = wp_get_current_user();
 		annowf_save_audit_item($post_id, $current_user->ID, 8, array($response['user']->ID));
-		
+
 	}
 	unset($response['user']);
 	echo json_encode($response);
@@ -501,7 +504,7 @@ function annowf_add_co_author() {
 	if ($response['message'] == 'success') {
 		// Used for quick access when filtering posts on a post-author page
 		$post_id = absint($_POST['post_id']);
-		
+
 		// Send email
 		if (anno_workflow_enabled('notifications')) {
 			$post = get_post($post_id);
@@ -510,7 +513,7 @@ function annowf_add_co_author() {
 
 		// Add author to JSON for appending to author dropdown
 		$response['author'] = '<option value="'.$response['user']->ID.'">'.$response['user']->user_login.'</option>';
-		
+
 		//Add to the audit log
 		$current_user = wp_get_current_user();
 		annowf_save_audit_item($post_id, $current_user->ID, 6, array($response['user']->ID));
@@ -523,7 +526,7 @@ add_action('wp_ajax_anno-add-co-author', 'annowf_add_co_author');
 
 /**
  * AJAX handler for adding a user to a post with a given type
- * 
+ *
  * @param string $type Type of user to add to the post (co_author, reviewer)
  * @return array Array of data pertaining to user added and JSON data.
  */
@@ -533,7 +536,7 @@ function annowf_add_user($type) {
 
 	$message = 'error';
 	$html = '';
-	
+
 	if (isset($_POST['post_id']) && isset($_POST['user'])) {
 		$user_login = trim($_POST['user']);
 		$user = get_user_by('login', $user_login);
@@ -545,19 +548,19 @@ function annowf_add_user($type) {
 				$user = $users[0];
 			}
 		}
-		
+
 		if (!empty($user)) {
 			$post = get_post($_POST['post_id']);
 			$co_authors = anno_get_authors(absint($_POST['post_id']));
 			$reviewers = anno_get_reviewers(absint($_POST['post_id']));
-			
+
 			if ($type == 'reviewer') {
 				$type_string = _x('reviewer', 'noun describing user', 'anno');
 			}
 			else {
 				$type_string = _x('co-author', 'noun describing user', 'anno');
 			}
-			
+
 			if ($post->post_author == $user->ID) {
 				$html = sprintf(_x('Cannot add author as a %s', 'Adding user error message for article meta box', 'anno'), $type_string);
 			}
@@ -584,19 +587,19 @@ function annowf_add_user($type) {
 
 /**
  * Handles AJAX request for remove a reviewer to a post.
- */ 
+ */
 function annowf_remove_reviewer() {
 	$response = annowf_remove_user('reviewer');
 	$response['decrement'] = 0;
 	if ($response['message'] == 'success') {
 		$post_id = absint($_POST['post_id']);
 		$user_id = absint($_POST['user_id']);
-		
+
 		// Send back to submitted state if we've removed all the reviewers
 		if (count(anno_get_reviewers($post_id)) == 0) {
 			update_post_meta($post_id, '_post_state', 'submitted');
 		}
-		
+
 		// Check if the user had already left a review and send back in response to update dom appropriately
 		$round = annowf_get_round($post_id);
 		$reviews = get_post_meta($post_id, '_round_'.$round.'_reviewed', true);
@@ -609,11 +612,11 @@ function annowf_remove_reviewer() {
 			update_post_meta($post_id, '_round_'.$round.'_reviewed', $reviews);
 			$response['decrement'] = 1;
 		}
-		
+
 		//Add to the audit log
 		$current_user = wp_get_current_user();
 		annowf_save_audit_item($post_id, $current_user->ID, 9, array($user_id));
-		
+
 	}
 	echo json_encode($response);
 	die();
@@ -626,7 +629,7 @@ add_action('wp_ajax_anno-remove-reviewer', 'annowf_remove_reviewer');
 function annowf_remove_co_author() {
 	$response = annowf_remove_user('co_author');
 	if ($response['message'] == 'success') {
-		if (isset($_POST['user_id'])) {	
+		if (isset($_POST['user_id'])) {
 			//Add to the audit log
 			$current_user = wp_get_current_user();
 			annowf_save_audit_item(absint($_POST['post_id']), $current_user->ID, 7, array(absint($_POST['user_id'])));
@@ -639,7 +642,7 @@ add_action('wp_ajax_anno-remove-co_author', 'annowf_remove_co_author');
 
 /**
  * AJAX handler for removing users from a post for a given type
- */ 
+ */
 function annowf_remove_user($type) {
 	check_ajax_referer('anno_manage_'.$type, '_ajax_nonce-manage-'.$type);
 	$response['message'] = 'error';
@@ -654,16 +657,16 @@ function annowf_remove_user($type) {
 /**
  * Fetch the post state of a given post. If no post state is found
  * it will be determined by the post status
- * 
+ *
  * @param int $post_id The id to fetch the post stat for
  * @return string Post state
- */ 
+ */
 function annowf_get_post_state($post_id) {
 	$post_state = get_post_meta($post_id, '_post_state', true);
 	if (!$post_state) {
 		$post = get_post($post_id);
 		if ($post) {
-			switch ($post->post_status) {			
+			switch ($post->post_status) {
 				case 'publish':
 					$post_state = 'published';
 					break;
@@ -682,10 +685,10 @@ function annowf_get_post_state($post_id) {
 /**
  * Return the round associated with a post. Rounds are determined by the number
  * of times they have been in the draft state. (sent for revisions)
- * 
+ *
  * @param int $post_id The id to fetch the post stat for
  * @return int Round
- */ 
+ */
 function annowf_get_round($post_id) {
 	$round = get_post_meta($post_id, '_round', true);
 	if (!$round) {
@@ -703,7 +706,7 @@ function annowf_author_meta_box($post) {
 		echo esc_html($author->user_login);
 	}
 	else {
-		$authors = anno_get_authors($post->ID);	
+		$authors = anno_get_authors($post->ID);
 ?>
 <label class="screen-reader-text" for="post_author_override"><?php _ex('Author', 'Author meta box dropdown label', 'anno'); ?></label>
 <?php
@@ -718,10 +721,10 @@ function annowf_author_meta_box($post) {
 
 /**
  * Admin request handler. Handles backend permission enforcement, cloning.
- */ 
+ */
 function annowf_admin_request_handler() {
 	global $anno_post_save, $post;
-	
+
 	// Cloning. This must come before the enforcing of capabilities below.
 	if (isset($_POST['publish']) && $_POST['publish'] == $anno_post_save['clone']) {
 		$post_id = anno_get_post_id();
@@ -731,7 +734,7 @@ function annowf_admin_request_handler() {
 		$new_id = annowf_clone_post($post_id);
 		if (!empty($new_id)) {
 			$url = add_query_arg('message', 11, get_edit_post_link($new_id, 'url'));
-		} 
+		}
 		else {
 			$url = add_query_arg('message', 12, get_edit_post_link($post_id, 'url'));
 		}
@@ -772,7 +775,7 @@ function annowf_admin_request_handler() {
 			$post_type = $post->post_type;
 		}
 	}
-		
+
 	if (!empty($wp_action) && !empty($post_type) && $post_type == 'article') {
 		switch ($wp_action) {
 			case 'postajaxpost':
@@ -799,7 +802,7 @@ function annowf_admin_request_handler() {
 				break;
 			case 'delete':
 				$anno_cap = 'admin';
-				break;			
+				break;
 			default:
 				break;
 		}
@@ -844,13 +847,13 @@ function annowf_user_has_cap_filter($user_caps) {
 
 /**
  * Adjust the edit display to override default WP implementation
- */ 
+ */
 function annowf_get_sample_permalink_html($return, $id, $new_title, $new_slug) {
 	$post = get_post($id);
 	if (!empty($post) && $post->post_type == 'article') {
 		if (anno_user_can('edit_slug', null, $id)) {
 			return $return;
-		} 
+		}
 		else {
 			return '';
 		}
@@ -861,8 +864,8 @@ add_filter('get_sample_permalink_html', 'annowf_get_sample_permalink_html', 10, 
 
 /**
  * If this article was imported, show a notice to the user, flag is removed on save.
- * 
- * 
+ *
+ *
  */
 function annowf_imported_admin_notices($empty) {
 	global $pagenow;
@@ -880,12 +883,12 @@ function annowf_imported_admin_notices($empty) {
 
 /**
  * Remove actions TD content if user cannot edit this post in the Workflow context
- * 
+ *
  * @todo Preferably a non-js way to do this. Currently js is the only option
- */ 
+ */
 function annowf_revision_remove_restore_link() {
 	// This is actually the original post, not the revision
-	global $post;	
+	global $post;
 	if ($post->post_type == 'article' && !anno_user_can('edit_post')) {
 ?>
 <script type="text/javascript">
