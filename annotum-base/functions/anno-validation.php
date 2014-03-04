f5a1bbebc9c52f6667c69a4b2e604a332d6f77c7
Evan Anderson <ejdanderson@gmail.com>
2014-03-04 12:38:40 -0700

    Abstracting out validation to be used for importer

--- annotum-base/functions/anno-validation.php
+++ annotum-base/functions/anno-validation.php
@@ -10,50 +10,50 @@
  * http://www.opensource.org/licenses/gpl-license.php
  */
 
-function anno_validate($content) {
+function anno_validate($content, $schema) {
+	$response = array();
+	$response['status'] = '';
+
+	define('XML_PARSE_BIG_LINES', 4194304); //@TODO incorporate this
 	$doc = new DOMDocument();
-	//$doc->loadxml($post->post_content_filtered);
+	libxml_use_internal_errors(true);
+	$doc->loadxml($content);
 
-	$doc->loadxml('<article></article>');
-
-	if (!$doc->relaxNGValidate(get_template_directory().'/js/textorum/schema/kipling-jp3.rng')) {
-    	print '<b>DOMDocument::schemaValidate() Generated Errors!</b>';
-    	//libxml_display_errors();
+	if (!$doc->relaxNGValidate($schema)) {
+		$response['status'] = 'error';
+		$errors = libxml_get_errors();
+		if (is_array($errors)) {
+			foreach ($errors as $libxml_error) {
+				$response['errors'][] = array(
+					'fullMessage' => sprintf(__('Error on line %d, column %d. %s', 'Error message for validation. %s referes to the error message', 'anno'), $libxml_error->line, $libxml_error->column, str_replace('\n', '', $libxml_error->message)),
+					// -1 here as the index starts at 0 but display starts at 1
+					'line' => $libxml_error->line - 1,
+					'column' => $libxml_error->column,
+					'message' => str_replace('\n', '', $libxml_error->message)
+				);
+			}
+		}
+	}
+	else {
+		$response['status'] = 'success';
 	}
 
+	return $response;
 }
 
 function anno_ajax_validate() {
 	$response = array();
-	define('XML_PARSE_BIG_LINES', 4194304);
+
 	if (isset($_POST['content'])) {
 		$content = wp_unslash($_POST['content']);
-		$doc = new DOMDocument();
-		libxml_use_internal_errors(true);
-		$doc->loadxml($content);
+		$schema = trailingslashit(get_template_directory()).'functions/schema/kipling-jp3-partial.rng';
+		$response = anno_validate($content, $schema);
 
-
-		if (!$doc->relaxNGValidate(trailingslashit(get_template_directory()).'functions/schema/kipling-jp3-partial.rng')) {
-			$response['status'] = 'error';
-			$errors = libxml_get_errors();
-			if (is_array($errors)) {
-				foreach ($errors as $libxml_error) {
-					$response['errors'][] = array(
-						'fullMessage' => sprintf(__('Error on line %d, column %d. %s', 'Error message for validation. %s referes to the error message', 'anno'), $libxml_error->line, $libxml_error->column, str_replace('\n', '', $libxml_error->message)),
-						// -1 here as the index starts at 0 but display starts at 1
-						'line' => $libxml_error->line - 1,
-						'column' => $libxml_error->column,
-						'message' => str_replace('\n', '', $libxml_error->message)
-					);
-				}
-			}
-		}
-		else {
+		if ($response['status'] == 'success') {
 			$response['status'] = 'success';
 			$response['message'] = __('Validation Successful', 'anno');
 		}
 	}
-	error_log(print_r($response,1));
 
 	echo json_encode($response);
 	die();
