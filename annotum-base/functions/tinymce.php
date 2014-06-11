a6e0298f698ed1d209e66281fb8e039cbee1cb72
Chris Mospaw <cmospaw@crowdfavorite.com>
2014-06-11 10:09:43 -0600

    #123 Remove section button from Abstract

--- annotum-base/functions/tinymce.php
+++ annotum-base/functions/tinymce.php
@@ -271,7 +271,7 @@ class Anno_tinyMCE {
 	function mce_buttons_2($buttons, $ed_id) {
 		if ($this->is_article()) {
 			if ($ed_id == 'excerpt') {
-				$buttons = array('bold,italic,underline,superscript,subscript,charmap,annosection');
+				$buttons = array('bold,italic,underline,superscript,subscript,charmap');
 			}
 			else {
 				$buttons = explode(',', 'annoorderedlist,annobulletlist,annooutdentlist,annoindentlist,bold,italic,underline,annopreformat,annomonospace,superscript,subscript,charmap,|,annolink,announlink,|,annosection,annosubsection,mysplitbutton,|,annoreferences,|,annoimages,|,annoquote,annoequations');
